d0a0a7e07ca84b7e9d28cc41b41a698b281df2af
shyslav <shyshkin.vladyslav@gmail.com>
2016-09-13 18:45:40 +0300

    added login controller

---
+++ src/main/java/com/sukhaniuk/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.sukhaniuk.controller;
+
+import com.sukhaniuk.storage.UsersStorage;
+import com.sukhaniuk.utils.GlobalController;
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+/**
+ * Created by shyshkin_vlad on 10.04.16.
+ */
+@SuppressWarnings("unused")
+@Controller
+public class LoginController extends GlobalController {
+    private static final Logger log = Logger.getLogger(LoginController.class.getName());
+
+    @RequestMapping(value = "login")
+    public String login(ModelMap map, HttpServletRequest request) throws IOException, JSONException, SQLException {
+        return "login";
+    }
+
+    @RequestMapping(value = "login/signin")
+    public String signin(ModelMap map, HttpServletRequest request) throws IOException, JSONException, SQLException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        UsersStorage.login(login,password);
+        return "login";
+    }
+
+    @RequestMapping(value = "login/signout")
+    public String signout(ModelMap map, HttpServletRequest request) throws IOException, JSONException, SQLException {
+        return "login";
+    }
+}
